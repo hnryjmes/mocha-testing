be42071283bc66adf6163aeeaa0de67fabf97d21
Henry Cooksley <henry.cooksley@gmail.com>
2019-01-14 14:32:17 +0000

    Rewrite to use async/await

--- test/promise-md5.test.js
+++ test/promise-md5.test.js
@@ -3,15 +3,14 @@ const promiseMd5 = require('../promise-md5');
 
 describe('#promiseMd5()', () => {
   context('with string argument', () => {
-    it('should compute MD5 hash', () => (
-      promiseMd5('Glad Chinda')
-        .then((hash) => {
-          expect(hash)
-            .to.be.a('string')
-            .that.matches(/^[a-f0-9]{32}$/)
-            .and.equal('877dbb93f50eb8a89012e15bd37ee7e4');
-        })
-    ));
+    it('should compute MD5 hash', async () => {
+      const hash = await promiseMd5('Glad Chinda');
+
+      expect(hash)
+        .to.be.a('string')
+        .that.matches(/^[a-f0-9]{32}$/)
+        .and.equal('877dbb93f50eb8a89012e15bd37ee7e4');
+    });
   });
 
   context('with non-string argument', () => {
